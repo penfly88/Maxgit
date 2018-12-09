26e8e98dd84cc393d2eff485898ba28bdfbc30f1
penfly88 <moriejay@gmail.com>
2018-12-09 16:50:05 +0200

    fifth commit

---
+++ wethinkcode/Day05/ex01/ft_strncpy1.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isekhwel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/28 16:39:37 by isekhwel          #+#    #+#             */
+/*   Updated: 2017/11/28 16:58:56 by isekhwel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	int counter;
+
+	counter = 0;
+	while ((counter <= n) && (src[counter] != '\0'))
+	{
+		dest[counter] = src[counter];
+		counter++;
+	}
+	while (counter < n)
+	{
+		dest[counter] = '\0';
+		counter++;
+	}
+	return (dest);
+}
